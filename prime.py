e38ea558f947ac067cbef41f4a2097ea1e9de630
abhimskywalker <malik.abhishek.iitk@gmail.com>
2013-01-29 20:28:03 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,28 @@
-
+import math
 
 def isPrime(n):
-    if n<=1 return false
-    if n==2 return true
-    int m=sqrt(n)
+    if n<=1: return False
+    if n==2: return True
+    m=round(math.sqrt(n))
     for i in range(3,m,2):
-        if n%i==0: return false
-    return true
+        if n%i==0: return False
+    return True
 
+def primeList(n):
+    
+
+'''public boolean[] sieve(int n)
+{
+   boolean[] prime=new boolean[n+1];
+   Arrays.fill(prime,true);
+   prime[0]=false;
+   prime[1]=false;
+   int m=Math.sqrt(n);
+
+   for (int i=2; i<=m; i++)
+      if (prime[i])
+         for (int k=i*i; k<=n; k+=i)
+            prime[k]=false;
+
+   return prime;
+} '''
